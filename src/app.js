83e252c7aedf4bce8818fb8c0c006f9ed1a011d9
BetPro-bit <cbhmediake@gmail.com>
2026-06-17 09:14:25 +0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.get('/health', (req, res) => res.json({ status: 'ok', time: new Date() }));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/payments', require('./routes/payments'));
 app.use('/api/games', require('./routes/games'));
+app.use('/api/football', require('./routes/football'));
 app.use('/api/callback', require('./routes/callback'));
 
 // 404
