25ff1c12b09d8a6f68de1f1bef08058f20e93140
BetPro-bit <cbhmediake@gmail.com>
2026-06-18 08:42:27 +0300

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const app = require('./src/app');
-const PORT = parseInt(process.env.PORT, 10) || 5000;
-
-app.listen(PORT, () => {
-  console.log(`BetPro Backend running on port ${PORT}`);
-});
